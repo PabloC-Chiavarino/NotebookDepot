13d43be67cb9b9ba8d9f33ac048564400f499821
Pablo C. Chiavarino <pchiavarino89@gmail.com>
2023-10-22 20:38:17 -0300

    [feature] scroll buttons components

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,15 +1,25 @@
+import { useRef } from 'react'
 import { useParams } from 'react-router-dom'
 import { useFirestore } from '../../hooks'
-import { Loader, ItemList, SectionTitle } from '../../components'
+import { Loader, ItemList, SectionTitle, BackBtn, FowardBtn } from '../../components'
 import './styles.css'
 
 const ItemListContainer = () => {
-    
+    const scrollContainer = useRef()
+
+    const scrollBack = () => {
+        scrollContainer.current.scrollLeft = -366
+    }
+
+    const scrollFoward = () => {
+        scrollContainer.current.scrollLeft = 400
+    }
+
     const { categoryId } = useParams()
     const { loading, data } = useFirestore('collection')
 
     return (
-        <section className='products__container'>
+        <section className='products__container' ref={scrollContainer}>
             {loading ? (
                 categoryId ? (
                     <Loader greeting={`Cargando ${categoryId}`} />
@@ -19,7 +29,9 @@ const ItemListContainer = () => {
             ) : (
                 <>
                     <SectionTitle section={categoryId ? categoryId : 'Productos Destacados' } />
+                    <BackBtn scrollBack={scrollBack}/>
                     <ItemList data={data} />
+                    <FowardBtn scrollFoward={scrollFoward}/>
                 </>
             )}
         </section>
